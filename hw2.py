617af110b71ba5d33a96e6d6209dec95d3471d4b
Ts1BR <126414351+Ts1BR@users.noreply.github.com>
2023-06-10 23:32:47 -0700

    Add files via upload

---
+++ hw2.py
@@ -0,0 +1,46 @@
+'''exercise 1.1'''
+
+#
+
+'''exercise 1.2'''
+
+# inp = int(input("Enter a positive number: "))
+
+# sum = 0
+
+# for i in range(1, inp + 1):
+#     sum += i
+
+# print("Sum is:", sum)
+
+'''exercise 1.3'''
+
+# listofnum = input("Input a list of 5 integers seperated by spaces: ")
+# numbers_list = listofnum.split()
+
+# numbers_list = [int(num)*int(num) for num in numbers_list]
+
+# print(numbers_list)
+
+'''exerise 2.1'''
+
+# v = ["a", "e", "i", "o", "u"]
+
+# ip = input("input a string: ")
+
+# count = sum(ip.count(i) for i in v)
+
+# print("The amount of vowels in the input was", count)
+
+'''exerise 2.2'''
+
+#
+
+'''exerise 2.3'''
+
+# list = input("Input a list of 5 strings seperated by spaces: ")
+# numbers_list = list.split()
+
+# numbers_list = [len(num) for num in numbers_list]
+
+# print(numbers_list)
